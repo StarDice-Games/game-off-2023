77007a6f6c734f58061a83a86e88b56f99700cc0
Guidi Simone <gsimone21892@gmail.com>
2023-11-04 22:29:06 +0100

    + reset button,

--- Global.gd
+++ Global.gd
@@ -105,4 +105,10 @@ func _process(delta):
 	#TODO remove this
 	if Input.is_action_just_pressed("ChangeLevelDebug"):
 		get_tree().change_scene_to_packed(scenes[next_scene])
+
+	if Input.is_action_just_pressed("ResetLevel"):
+		if next_scene == 0:
+			next_scene = scenes.size()
+		
+		get_tree().change_scene_to_packed(scenes[next_scene - 1])
 	pass
