e86fa9eb1f5631894d11c48f1647803b62c8e9ea
Guidi Simone <gsimone21892@gmail.com>
2023-11-11 17:12:25 +0100

    + added block door, that can be opened from triggers

---
+++ Scenes/PlatformerPlayer/Doors/BlockDoor/BlockDoor.gd
@@ -0,0 +1,33 @@
+extends StaticBody2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func fullyOpen():
+	hide()
+	$CollisionShape2D.disabled = true
+	$CollisionShape2D.process_mode = Node.PROCESS_MODE_DISABLED
+	pass
+
+func fullyClosed():
+	$CollisionShape2D.disabled = false
+	$CollisionShape2D.process_mode = Node.PROCESS_MODE_INHERIT
+	pass
+	
+func _on_receiver_component_enabled():
+	#open the door , play a little animation at the end remove the collider
+	$AnimationPlayer.play("open")
+	pass # Replace with function body.
+
+
+
+func _on_receiver_component_disabled():
+	show()
+	$AnimationPlayer.play("close")
+	pass # Replace with function body.
