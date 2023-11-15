db975fcd03663964cce7dfacd6031973112cf020
Guidi Simone <gsimone21892@gmail.com>
2023-11-15 23:25:07 +0100

    + floating box

--- Scenes/PlatformerPlayer/Pickable/Box/BoxPickable.gd
+++ Scenes/PlatformerPlayer/Pickable/Box/BoxPickable.gd
@@ -5,11 +5,20 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+var afloat = false
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 	# Add the gravity.
-	if not is_on_floor() and $PickupComponent.held == false:
+	if not is_on_floor() and $PickupComponent.held == false and not afloat:
 		velocity.y += gravity * delta
 	move_and_slide()
+	var collision = get_last_slide_collision()
+	if collision != null:
+		var collider = collision.get_collider()
+		if collider != null and collider is WaterFloor:
+			afloat = true
+			remove_from_group("Pickable")
+			$AnimationPlayer.play("float")
