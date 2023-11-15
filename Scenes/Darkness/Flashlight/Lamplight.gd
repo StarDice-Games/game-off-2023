87e4a62687a2315fb2e3c4e617a39a8325961136
Guidi Simone <gsimone21892@gmail.com>
2023-11-15 22:19:49 +0100

    + last code on lamplight

--- Scenes/Darkness/Flashlight/Lamplight.gd
+++ Scenes/Darkness/Flashlight/Lamplight.gd
@@ -1,3 +1,4 @@
+@tool
 extends CharacterBody2D
 class_name Lamplight
 
@@ -5,6 +6,31 @@ class_name Lamplight
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @export var side = 0
+@export var laserLength = 100
+
+@onready var rayCast = $RayCast2D
+@onready var line = $Line2D
+
+func _ready():
+	rayCast.target_position = Vector2(laserLength, 0)
+#	line.clear_points()
+#	line.add_point(position)
+#	line.add_point(rayCast.target_position)
+	
+func _process(delta):
+#	rayCast.target_position = Vector2(laserLength, 0)
+	line.clear_points()
+	
+	line.add_point(position)
+	
+	var collisionPoint = rayCast.get_collision_point()
+	if collisionPoint != Vector2.ZERO:
+		line.add_point(collisionPoint)
+	else:
+		line.add_point(to_global(Vector2.ZERO))
+	pass
+	
+	
 
 func _physics_process(delta):
 	# Add the gravity.
