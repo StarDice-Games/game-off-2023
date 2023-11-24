e5309d0b3877bb8edc56b884fa9c604ab3fd470f
Guidi Simone <gsimone21892@gmail.com>
2023-11-24 21:54:14 +0100

    + global audio manager for sfx

--- AudioManager.gd
+++ AudioManager.gd
@@ -1,9 +1,11 @@
 extends Node
 
-var num_players = 8
-var bus = "master"
+@export var num_players = 8
+@export var bus = "sfx"
+@export var mute = false
 
 var available = []  # The available players.
+#TODO fix how the queue is managed
 var queue = []  # The queue of sounds to play.
 
 
@@ -11,26 +13,33 @@ func _ready():
 	# Create the pool of AudioStreamPlayer nodes.
 	for i in num_players:
 		var p = AudioStreamPlayer.new()
-		add_child(p)
-		available.append(p)
 		p.connect("finished", _on_stream_finished)
 		p.bus = bus
+		add_child(p)
+		available.append(p)
 
 
-func _on_stream_finished(stream):
+func _on_stream_finished():
 	# When finished playing a stream, make the player available again.
-	available.append(stream)
+#	available.append()
+	pass
 
 
 #func play(sound_path):
 #	queue.append(sound_path)
 
 func play(sound : AudioStream):
-	queue.append(sound)
+	if sound != null:
+		queue.append(sound)
 
 func _process(delta):
 	# Play a queued sound if any players are available.
-	if not queue.is_empty() and not available.is_empty():
-		available[0].stream = queue.pop_front()
-		available[0].play()
-		available.pop_front()
+	if not queue.is_empty():
+		var notPlaying = available.filter(isStreamStopped)
+		if not notPlaying.is_empty():
+			var player = notPlaying.pop_front()
+			player.stream = queue.pop_front()
+			player.play()
+
+func isStreamStopped(player):
+	return not player.playing
