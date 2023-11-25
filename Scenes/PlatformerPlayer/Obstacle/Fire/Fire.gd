4321019dc5771b49cd6a27bd1f32f6fc803815f1
Guidi Simone <gsimone21892@gmail.com>
2023-11-25 08:48:42 +0100

    + fire sound and put off sound

--- Scenes/PlatformerPlayer/Obstacle/Fire/Fire.gd
+++ Scenes/PlatformerPlayer/Obstacle/Fire/Fire.gd
@@ -1,9 +1,13 @@
 extends StaticBody2D
 
+@export_category("Audio")
+@export var fireSound : AudioStream
+@export var putOutSound : AudioStream
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimatedSprite2D.play("default")
+	AudioManager.play(fireSound)
 	pass # Replace with function body.
 
 
@@ -14,6 +18,7 @@ func _process(delta):
 func extinguish(node):
 	for group in node.get_groups():
 		if group == "Water":
+			AudioManager.play(putOutSound)
 			get_tree().queue_delete(self)
 
 func _on_area_2d_area_entered(area):
