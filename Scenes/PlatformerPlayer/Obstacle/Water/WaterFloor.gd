93c4c74c4fa9f3003fb24492088f42fc5e155ca7
Guidi Simone <gsimone21892@gmail.com>
2023-11-25 11:52:39 +0100

    + water plop sfx, and pipe sound

--- Scenes/PlatformerPlayer/Obstacle/Water/WaterFloor.gd
+++ Scenes/PlatformerPlayer/Obstacle/Water/WaterFloor.gd
@@ -1,12 +1,26 @@
 extends StaticBody2D
 class_name WaterFloor
 
+@export_category("Audio")
+@export var plopSound : AudioStream
+@export var pipeSound : AudioStream
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	AudioManager.play(pipeSound)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_area_2d_area_entered(area):
+	AudioManager.play(plopSound)
+	pass # Replace with function body.
+
+
+func _on_area_2d_body_entered(body):
+	AudioManager.play(plopSound)
+	pass # Replace with function body.
