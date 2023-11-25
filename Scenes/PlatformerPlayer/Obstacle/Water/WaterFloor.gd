620a1a890f9f99e3c9e071ed82996eadd6f9fb2b
Guidi Simone <gsimone21892@gmail.com>
2023-11-25 15:09:33 +0100

    + prevent water from playing the plop sound multiple times

--- Scenes/PlatformerPlayer/Obstacle/Water/WaterFloor.gd
+++ Scenes/PlatformerPlayer/Obstacle/Water/WaterFloor.gd
@@ -5,6 +5,8 @@ class_name WaterFloor
 @export var plopSound : AudioStream
 @export var pipeSound : AudioStream
 
+var mapSplash = {}
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	AudioManager.play(pipeSound)
@@ -17,10 +19,16 @@ func _process(delta):
 
 
 func _on_area_2d_area_entered(area):
-	AudioManager.play(plopSound)
+	var key = area.get_parent().get_path().get_concatenated_names()
+	if not mapSplash.has(key):
+		AudioManager.play(plopSound)
+		mapSplash[key] = area.name
 	pass # Replace with function body.
 
 
 func _on_area_2d_body_entered(body):
-	AudioManager.play(plopSound)
+	var key = body.get_path().get_concatenated_names()
+	if not mapSplash.has(key):
+		AudioManager.play(plopSound)
+		mapSplash[key] = body.name
 	pass # Replace with function body.
