73c6c09e8433d874dc4a9954eaec41b7ef2cbfe5
Guidi Simone <gsimone21892@gmail.com>
2023-11-26 20:42:32 +0100

    + pressure plate animation and sound

--- Scenes/PlatformerPlayer/PressurePlates/PressurePlateComponent.gd
+++ Scenes/PlatformerPlayer/PressurePlates/PressurePlateComponent.gd
@@ -2,7 +2,12 @@ extends Area2D
 class_name PressurePlateComponent
 
 @export var receivers : Array[Node2D]
+@export_category("Audio")
+@export var compressSound : AudioStream
+@export var decompressSound : AudioStream
+
 @onready var senderComp : SenderComponent = $SenderComponent
+@onready var animator : AnimationPlayer = $AnimationPlayer
 
 var numOfObject = 0;
 
@@ -20,6 +25,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
 	pass
 
 func _on_area_entered(area):
@@ -30,6 +36,9 @@ func _on_area_entered(area):
 func _on_body_entered(body):
 	print("%s _on_body_entered:" % name)
 	senderComp.present(true)
+	if numOfObject <= 0:
+		animator.play("push")
+		AudioManager.play(compressSound)
 	numOfObject += 1
 	pass # Replace with function body.
 
@@ -38,6 +47,8 @@ func _on_body_exited(body):
 	numOfObject -= 1
 	if numOfObject <= 0:
 		senderComp.present(false)
+		animator.play("release")
+		AudioManager.play(decompressSound)
 	pass # Replace with function body.
 
 func _on_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
